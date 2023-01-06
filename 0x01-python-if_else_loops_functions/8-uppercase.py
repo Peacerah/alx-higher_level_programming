022ec9763df2afcabfc5802cc0fc49a846666c47
Peacerah <adektunji@gmail.com>
2023-01-06 17:39:55 +0000

    8-uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
         n = ord(str[c])
         if (n >= 97 and n <= 122):
             n -= 32
-        print(f"{chr(n)}", end="")
+        print("{}".format(chr(n), end="")
     print()
