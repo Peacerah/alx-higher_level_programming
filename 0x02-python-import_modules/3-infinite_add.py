c8004138e10b09693e7a0fc1f72861e4073dec7a
Peacerah <adektunji@gmail.com>
2023-01-07 17:15:29 +0000

    3-infinite edited

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 if __name__ == "__main__":
-    import sys
-    res = 0
-    for str in range(len(sys.argv)):
-        if (str == 0):
-            continue
+	import sys
+	res = 0
+	if len(sys.argv) != 0    
+		for str in range(len(sys.argv)):
+			res += int(sys.argv[str])
+	print("{}".format(res))
         else:
-            res += int(sys.argv[str])
-    print("{}".format(res))
+		print(0)
